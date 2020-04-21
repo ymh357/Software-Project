92c2300d6f150eb2a9c661fc1697d27429882225
yaokk <yaowg539@gmail.com>
2020-04-21 21:15:33 +1000

    remove useless middleware

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
- 
-module.exports = function(app) {
-  app.use('/api',createProxyMiddleware({ target: 'http://52.68.78.115:5000',changeOrigin: true, }));
-  // app.use(proxy('/test', { target: 'http://localhost:3000' }));
-};
