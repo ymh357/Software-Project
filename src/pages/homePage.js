9d86e9ea8f90034db15e652c25ad29099c8c7c07
nbd <minghaoy1@student.unimelb.edu.au>
2020-04-21 11:41:44 +0000

    tiny

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -14,7 +14,7 @@ class HomePage extends React.Component{
                     <ul className={homeStyle.menu}>
                         <li>
                             <img src='https://image.flaticon.com/icons/svg/651/651998.svg' alt={'scan img'}/>
-                            <Link to={'/'}>SCAN</Link>
+                            <Link to={'/scan'}>SCAN</Link>
                         </li>
                         <li>
                             <img src='https://as2.ftcdn.net/jpg/02/02/10/47/500_F_202104739_twqUgB0XofzZyEd17gHi81nJvOhhpZ9Z.jpg' alt={'manually img'}/>
@@ -22,7 +22,7 @@ class HomePage extends React.Component{
                         </li>
                         <li>
                             <img src='https://image.flaticon.com/icons/svg/711/711897.svg' alt={'cart img'}/>
-                            <Link to={'/'}>CART</Link>
+                            <Link to={'/cart'}>CART</Link>
                         </li>
                         <li>
                             <img src='https://image.flaticon.com/icons/svg/2521/2521625.svg' alt={'history img'}/>
