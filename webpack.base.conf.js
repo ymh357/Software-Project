1903e91c284c1c0c52d05d1a439da9b7806c49a6
angy <angelica.adornobenitez@student.unimelb.edu.au>
2020-04-24 11:06:07 +1000

    Update homePage.js
    cart button linked

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -32,11 +32,11 @@ module.exports = {
         ]
     },
     devServer: {
-        host: '0.0.0.0',
+        host: '127.0.0.1',
         historyApiFallback: true,
         proxy: {
         '/api/*':{
-            target: 'http://localhost:5000',
+            target: 'htt://52.68.78.115:5000',
             changeOrigin: true,
             secure: false
         }
