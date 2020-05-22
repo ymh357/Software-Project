494ddcb502c0ba4d885360331eb67ca8126faaf3
angy <angelica.adornobenitez@student.unimelb.edu.au>
2020-05-22 18:29:21 +1000

    product layout

--- src/index.js
+++ src/index.js
@@ -10,41 +10,7 @@ import CurrentOrderPage from './pages/currentOrderPage';
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        if(!localStorage.getItem('orders')){
-            let orders = [{
-                id: 1,
-                date: '04-04-2020',
-                products: [
-                    {id: 1, name: 'product A', quantity: 1, price: 10},
-                    {id: 2, name: 'product B', quantity: 2, price: 20},
-                ]
-            },
-                {
-                    id: 2,
-                    date: '05-04-2020',
-                    products: [
-                        {id: 2, name: 'product B', quantity: 3, price: 30},
-                        {id: 3, name: 'product C', quantity: 4, price: 40},
-                        {id: 4, name: 'product D', quantity: 1, price: 10},
-                    ]
-                }, {
-                    id: 3,
-                    date: '06-04-2020',
-                    products: [
-                        {id: 5, name: 'product E', quantity: 3, price: 30},
-                        {id: 6, name: 'product F', quantity: 4, price: 40},
-                        {id: 7, name: 'product G', quantity: 1, price: 10},
-                        {id: 8, name: 'product H', quantity: 1, price: 10},
-                    ]
-                }]
-            localStorage.setItem('orders', JSON.stringify(orders))
-        }
-        if(!localStorage.getItem('current_order')){
-
-            localStorage.setItem('current_order', JSON.stringify({
-                products:[]
-            }))
-        }
+        
     }
 
     render() {
