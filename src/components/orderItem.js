1de1224f49334fbf379f047313f0315d94e35d7f
yaokk <yaowg539@gmail.com>
2020-05-25 16:14:39 +1000

    update the data format on history page

--- src/components/orderItem.js
+++ src/components/orderItem.js
@@ -3,15 +3,48 @@ import {withRouter} from 'react-router-dom'
 
 class OrderItem extends React.Component{
 
+
     render(){
         const price = this.props.order.products.reduce((acc, cur) => {
             return acc + cur.priceTotalExTax
         },0)
+        
+        const date = ()=>{
+            let itemdate = new Date(this.props.order.createdDate)
+            //console.log(itemdate)
+            var seperator1 = "-";
+            var seperator2 = ":";
+            var month = itemdate.getUTCMonth() + 1;
+            var strDate = itemdate.getUTCDate();
+            if (month >= 1 && month <= 9) {
+            month = "0" + month;
+            }
+            if (strDate >= 0 && strDate <= 9) {
+            strDate = "0" + strDate;
+            }
+            var Hours = itemdate.getUTCHours();
+            var Minutes = itemdate.getUTCMinutes();
+            var Seconds = itemdate.getUTCSeconds();
+            if (Hours >= 0 && Hours <= 9) {
+                Hours = "0" + Hours;
+            }
+            if (Minutes >= 0 && Minutes <= 9) {
+            Minutes = "0" + Minutes;
+            }
+            if (Seconds >= 0 && Seconds <= 9) {
+            Seconds = "0" + Seconds;
+            }
+            var currentdate = itemdate.getUTCFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + Hours + seperator2 + Minutes
+            + seperator2 + Seconds;
+            return currentdate;
+        }
+
         return (
             <tr>
                 <td>{this.props.order.keyPurchaseOrderID}</td>
-                <td>{this.props.order.createdDate}</td>
-                <td>{this.props.order.bill_status}</td>
+                <td>{date()}</td>
+                <td>{this.props.order.bill_status.slice(7)}</td>
                 <td>{price.toFixed(2)}</td>
                 <td><a href={`/order_detail_${this.props.order.keyPurchaseOrderID}`}>Have a look</a></td>
             </tr>
